bd60c8e6f94619e64458a358aa473208ad98e981
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-02 07:32:09 +0100

    Added get all flight details successfully

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const { json } = require("express");
-const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
-const routes = require("./routes/flightRoute");
+// const flights = require("./controllers/flightController");
+ const flights = require("./models/Flight");
+// const routes = require("./routes/flightRoute");
 
 const app = express();
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
 
-app.use(json());
+ // It fetches all the fligths available start here
+app.get('/flights', (request, response) => response.json(flights))
+ //All flights details ends here
 
-app.use("/", routes);
+ // Get single flight details from the client's request
+app.get('/flights/:id', (request, response) => {
+        let id = request.params.id
+        let foundFlight = flights.find(flight => {
+          return flight.id === parseInt(id)
+        })
+          if(foundFlight) {
+            return response.status(200).json({Message: "Your flight id is found"})
+          } else{
+            return response.status(404).json({message: "Flight is not found"})
+          }
 
-app.get('/', (request, response) => {
-   response.send("kayode kayode owolabi jesuseun kayode")
-  
+          
+         // fetch a single flight details from the client's request.
+         // display error to client if erro occur from the server.
+         // response.status(500)
 })
-
 
 const port = process.env.PORT || 3000;
 
--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,81 @@
-exports.exampleModel = [];
+exports.flightModel = [
+    {
+        id :   1,
+        title: "flight to canada",
+        time: "1pm",
+        price: 26000,
+        date: "26-06-2022"
+        },
+
+        {
+            id:  2,
+            title: "flight to America",
+            time: "2pm",
+            price: 40000,
+            date: "26-07-2022"
+            },
+
+            {
+                id: 3,
+                title: "flight to Nigeria",
+                time: "8am",
+                price: 15000,
+                date: "26-08-2022"
+                },
+
+                {
+                    id: 4,
+                    title: "flight to Kenya",
+                    time: "6pm",
+                    price: 18000,
+                    date: "26-09-2022"
+                    },
+
+                    {
+                        id: 5,
+                        title: "flight to Dubai",
+                        time: "3pm",
+                        price: 24000,
+                        date: "26-10-2022"
+                        },
+
+                        {
+                            id: 6,
+                            title: "flight to Italy",
+                            time: "5pm",
+                            price: 50000,
+                            date: "26-11-2022"
+                            },
+
+                            {
+                                id: 7,
+                                title: "flight to Sweeden",
+                                time: "7pm",
+                                price: 60000,
+                                date: "26-02-2023"
+                                },
+
+                                {
+                                    id: 8,
+                                    title: "flight to Norway",
+                                    time: "12pm",
+                                    price: 100000,
+                                    date: "26-12-2022"
+                                    },
+
+                                    {
+                                        id: 9,
+                                        title: "flight to United Kingdom",
+                                        time: "1pm",
+                                        price: 45000,
+                                        date: "26-01-2023"
+                                        },
+
+                                        {
+                                            id: 10,
+                                            title: "flight to Dublin",
+                                            time: "5pm",
+                                            price: 75000,
+                                            date: "26-02-2023"
+                                            }
+];
