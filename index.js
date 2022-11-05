3cae959805eb729faa019db43b58483d76e76e5e
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-05 08:59:59 +0100

    Added a single, update, create and delete flight

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
 const express = require("express");
-const { json } = require("express");
-// const flights = require("./controllers/flightController");
- const flights = require("./models/Flight");
-// const routes = require("./routes/flightRoute");
-
+const {json} = require("express");
+const flights = require("./models/flight");
+const fs = require("fs");
 const app = express();
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- // It fetches all the fligths available start here
+ // It fetches all the flights available start here
 app.get('/flights', (request, response) => response.json(flights))
- //All flights details ends here
+ //Fetch all flights available details ends here
 
- // Get single flight details from the client's request
+ //Get single flight details from the client's request
 app.get('/flights/:id', (request, response) => {
-        let id = request.params.id
-        let foundFlight = flights.find(flight => {
-          return flight.id === parseInt(id)
-        })
-          if(foundFlight) {
-            return response.status(200).json({Message: "Your flight id is found"})
-          } else{
-            return response.status(404).json({message: "Flight is not found"})
-          }
+   const found = flights.some(flight => flight.id === parseInt(request.params.id));
 
-          
-         // fetch a single flight details from the client's request.
-         // display error to client if erro occur from the server.
-         // response.status(500)
+   if(found) {
+     response.json(flights.filter(flight => {
+      return flight.id === parseInt(request.params.id)
+     }))
+   }   else {
+     response.status(400).json();
+   }
+})
+  
+ // Creat new flight
+app.post('/flights', (request, response) => {
+    const newFlight = {
+      id:  request.body.id,
+      title: request.body.title,
+      time: request.body.time,
+      price: 34000,
+      date: request.body.date
+    }
+    flights.push(newFlight);
+    let stringed = JSON.stringify(flights);
+    fs.appendFile("./models/flight.js", stringed, (err) => {
+      if(err) {
+        return response.status(500).json({message: err});
+      }
+    })
+       // return response.json(flights);
+       return response.status(200).json({message: "new"});
+});
+
+    // update flight
+app.put('/flights/:id', (request, response) => {
+  const found = flights.some(flight => flight.id === parseInt(request.params.id));
+  if (found) {
+    const updateFlight = request.body;
+    flights.forEach(flight => {
+      if(flight.id === parseInt(request.params.id)) {
+         flight.title = updateFlight.title
+         flight.date = updateFlight.date
+         flight.time = updateFlight.time
+         response.json({msg: 'flight updated', flight});
+      }
+    })
+  } else {
+    response.status(400).json({msg: `No flight with id of ${request.params.id}`})
+  } 
+})
+
+//Delete flight 
+app.delete('/flights/:id', (request, response) => {
+  const found = flights.some(flight => flight.id === parseInt(request.params.id));
+
+  if(found) {
+    response.json( flights.filter(flight => {
+      return flight.id !== parseInt(request.params.id)
+    }))
+  }   else {
+    response.status(400).json({msg: `No flight with the Id ${request.params.id}`});
+  }
 })
 
 const port = process.env.PORT || 3000;
